3e6afe8ee07c75d5f14b1e10b6ebff1554a00d84
Ivan <tylergraip@gmail.com>
2013-01-23 01:21:17 +0200

    getting root projects, modified sample data

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -5,8 +5,26 @@ chrome.extension.sendMessage({}, function(response) {
 
 		// ----------------------------------------------------------
 		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
+		console.log("Hello. This message was sent from Woflowex");
 		// ----------------------------------------------------------
+
+		var Projects = [];
+
+                // make it recursive
+                $('.mainTreeRoot').find('.children .project')
+                  .each(function(){
+                    var project = $(this);
+                    var projectData = {};
+
+                    projectData.projectid = project.attr('projectid');
+                    projectData.name      = project.find('.name .content').html();
+                    projectData.notes     = project.find('.notes .content').html();
+ 
+                    Projects.push( projectData );
+                  });
+
+		// save this somewhere
+                console.log(Projects);
 
 	}
 	}, 10);
